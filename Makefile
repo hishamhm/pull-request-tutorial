846f2f4342bb82342218c4ced4c5dee40e04da79
Hisham <hisham@gobolinux.org>
2017-04-01 16:07:52 -0300

    Let's make PRs more fun :)

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 
+# This makefile also needs to be fixed! Can you send a PR to improve it? :)
+
 hello:
 	gcc -o hello hello.c
--- hello.c
+++ hello.c
@@ -3,7 +3,24 @@
 
 int main(int argc, char** argv) {
 
-   printf("Hello, woldr!\n");
+   printf("This tutorial was completed by:\n\n");
+
+   printf("@eneskaya\n");
+   printf("@yogs-kum\n");
+   printf("@gkarlos\n");
+   printf("@roxierox\n");
+   printf("@santushet\n");
+   printf("@praveenkumarb\n");
+   printf("@mkeohane-cisco\n");
+   printf("@mario1ua\n");
+   printf("@muhammadhassan102\n");
+   printf("@pdubois\n");
+   printf("@Curt-T\n");
+   printf("@azru0512\n");
+   printf("@ddferrando\n");
+   printf("@luotaoyeah\n");
+
+   printf("\nSend a PR to add your username here! :-) \n");
 
    return 0;
 }
